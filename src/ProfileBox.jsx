92a9b48d68bf2556f4014a9550cbd3f5bb79ba1d
dourbinav <140148404+dourbinav@users.noreply.github.com>
2024-07-08 09:53:38 +0530

    Update ProfileBox.jsx

--- src/ProfileBox.jsx
+++ src/ProfileBox.jsx
@@ -5,7 +5,7 @@ export default function ProfileBox() {
   return (
     <div className='w-64 bg-black text-white  '>
         <span className='m-2  flex flex-col font-semibold'>
-        <span><Link to="/signup" > <span>Signup</span> </Link>   || <Link to="/login" >    <span>Login</span>  </Link> </span>
+        <span><Link to="/signup" > <span>Signup</span> </Link>   || <Link to="/" >    <span>Login</span>  </Link> </span>
         <span>My Cart</span>
         <span>Wishlist</span>
         <span>Sales</span>
